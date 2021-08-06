aeb6ef2f400102cc8aef9a2a00921350ef9a518f
Beroy_Jenkins <gamarra.eduardo@gmail.com>
2021-08-06 17:28:02 -0300

    Finalizando tarefa
    criando docker compose e dockerfile, alterando scripts do main.py e adicionando extensão .txt no arquivo requirements.

--- tarefa/codigo/main.py
+++ tarefa/codigo/main.py
@@ -1,13 +1,32 @@
 import pickle
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+
 
 app = FastAPI()
 @app.post('/model')
 ## Coloque seu codigo na função abaixo
-def titanic(Sex:int):
+
+#definindo os parametros e os limites
+def titanic(Lifeboat:int, Sex:int= Query(..., ge=0, le=1), Age:float= Query(..., ge=1),  Pclass:int= Query(..., ge=1, le=3)):
     with open('model/Titanic.pkl', 'rb') as fid: 
         titanic = pickle.load(fid)
+        #titanic recebe 'DecisionTreeClassifier(random_state=1)'
+
+#tenta retornar respostas
+    try:
+        return {
+            'survived': bool(titanic.predict([[Sex,Age,Lifeboat,Pclass]])),
+            'status': 200,
+            'message': 'OK',
+        }
+    except Exception:
+        return {
+            'status': 500,
+            'message': "Internal Server Error"
+        }
+
 
 @app.get('/model')
 def get():
     return {'hello':'test'}
+    
